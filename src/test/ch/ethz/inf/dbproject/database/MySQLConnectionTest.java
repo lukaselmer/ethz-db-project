e30b1f2ac0843cd30e9538623f1cedd213da423f
Lukas Elmer <lukas.elmer@gmail.com>
2013-03-11 14:57:06 +0100

    test database connection

---
+++ src/test/ch/ethz/inf/dbproject/database/MySQLConnectionTest.java
@@ -0,0 +1,24 @@
+package test.ch.ethz.inf.dbproject.database;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import ch.ethz.inf.dbproject.database.MySQLConnection;
+
+public class MySQLConnectionTest {
+
+	@Test
+	public void testGetInstance() throws SQLException {
+		MySQLConnection c = MySQLConnection.getInstance();
+		assertNotNull(c);
+		Connection con = c.getConnection();
+		assertNotNull(con);
+		assertTrue("Database connection is not valid!", con.isValid(1));
+		assertFalse("Database is not writable!", con.isReadOnly());
+	}
+
+}
