565e5b5dd62b6aa126a7b51582b72ff4560c5cc7
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2013-03-27 14:11:58 +0100

    rename projects to project

--- src/main/java/ch/ethz/inf/dbproject/database/DatabaseSeeder.java
+++ src/main/java/ch/ethz/inf/dbproject/database/DatabaseSeeder.java
@@ -16,32 +16,40 @@ public class DatabaseSeeder {
 	}
 
 	protected static void seed() throws SQLException {
-		Statement s = con.createStatement();
-		createTables(s);
-		seedData(s);
-		s.close();
+//		Statement s = con.createStatement();
+//		createTables(s);
+//		seedData(s);
+//		s.close();
+		
+		// TODO: Load createTable sql script
+
+		
+		// TODO: Load insertData sql script
+		
+		
+		
 	}
 
-	private static void createTables(Statement s) throws SQLException {
-		createTable(s, "users", "name varchar(255) not null");
-		createTable(s, "projects", "user_id int not null, name varchar(255) not null", "key index_projects_on_user_id (user_id), unique (name)");
-		createTable(s, "cities", "name varchar(255) not null", "unique (name)");
-	}
-
-	private static void createTable(Statement s, String table, String vars, String additions) throws SQLException {
-		additions = additions == null ? "" : ", " + additions;
-		s.execute("create table " + table + " (id int not null, " + vars + ", primary key ( id )" + additions + ")");
-	}
-
-	private static void createTable(Statement s, String table, String vars) throws SQLException {
-		createTable(s, table, vars, null);
-	}
-
-	private static void seedData(Statement s) throws SQLException {
-		s.execute("insert into users values (1, 'Fred'), (2, 'Ivo'), (3, 'Luke')");
-		s.execute("insert into cities values (1, 'Zurich'), (2, 'Bern'), (3, 'Geneva')");
-		s.execute("insert into projects values (1, 3, 'HSR'), (2, 2, 'ETH'), (3, 1, 'World domination'), (4, 1, 'Foo')");
-	}
+//	private static void createTables(Statement s) throws SQLException {
+//		createTable(s, "users", "name varchar(255) not null");
+//		createTable(s, "projects", "user_id int not null, name varchar(255) not null", "key index_projects_on_user_id (user_id), unique (name)");
+//		createTable(s, "cities", "name varchar(255) not null", "unique (name)");
+//	}
+//
+//	private static void createTable(Statement s, String table, String vars, String additions) throws SQLException {
+//		additions = additions == null ? "" : ", " + additions;
+//		s.execute("create table " + table + " (id int not null, " + vars + ", primary key ( id )" + additions + ")");
+//	}
+//
+//	private static void createTable(Statement s, String table, String vars) throws SQLException {
+//		createTable(s, table, vars, null);
+//	}
+//
+//	private static void seedData(Statement s) throws SQLException {
+//		s.execute("insert into users values (1, 'Fred'), (2, 'Ivo'), (3, 'Luke')");
+//		s.execute("insert into cities values (1, 'Zurich'), (2, 'Bern'), (3, 'Geneva')");
+//		s.execute("insert into projects values (1, 3, 'HSR'), (2, 2, 'ETH'), (3, 1, 'World domination'), (4, 1, 'Foo')");
+//	}
 
 	protected static void reset() throws SQLException {
 		Statement s = con.createStatement();
