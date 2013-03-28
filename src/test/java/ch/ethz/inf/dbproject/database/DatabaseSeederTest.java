e864a1644edf6debedddd6f3b7640f8982053ecf
Lukas Elmer <lukas.elmer@gmail.com>
2013-03-29 00:33:39 +0100

    load schema and data from file, reimplemented tests

--- src/test/java/ch/ethz/inf/dbproject/database/DatabaseSeederTest.java
+++ src/test/java/ch/ethz/inf/dbproject/database/DatabaseSeederTest.java
@@ -17,49 +17,50 @@ import ch.ethz.inf.dbproject.exceptions.InvalidStateException;
 
 public class DatabaseSeederTest {
 
-	private static final List<String> EXPECTED_TABLES = Arrays.asList(new String[] { "users", "projects", "cities" });
+	private static final List<String> EXPECTED_TABLES = Arrays.asList(new String[] { "city", "category", "user", "project", "stretch_goal",
+			"funding_amount", "fund", "comment" });
 
 	@Before
 	public void reset() throws SQLException {
-		//DatabaseSeeder.resetAndSeed();
+		DatabaseSeeder.resetAndSeed();
 	}
 
 	@AfterClass
 	public static void resetAll() throws SQLException {
-		//DatabaseSeeder.resetAndSeed();
+		DatabaseSeeder.resetAndSeed();
 	}
 
 	@Test
 	public void testReset() throws SQLException {
-		//assertTrue("database not should be empty", DatabaseHelper.getTables().size() > 0);
-		//DatabaseSeeder.reset();
-		//assertTrue("database should be empty", DatabaseHelper.getTables().size() == 0);
+		assertTrue("database not should be empty", DatabaseHelper.getTables().size() > 0);
+		DatabaseSeeder.reset();
+		assertTrue("database should be empty", DatabaseHelper.getTables().size() == 0);
 	}
 
 	@Test
 	public void testSeed() throws SQLException {
-//		DatabaseSeeder.reset();
-//		assertTrue("database should be empty", DatabaseHelper.getTables().size() == 0);
-//		DatabaseSeeder.seed();
-//		assertTrue("database not should be empty", DatabaseHelper.getTables().size() > 0);
+		DatabaseSeeder.reset();
+		assertTrue("database should be empty", DatabaseHelper.getTables().size() == 0);
+		DatabaseSeeder.seed();
+		assertTrue("database not should be empty", DatabaseHelper.getTables().size() > 0);
 	}
 
 	@Test
 	public void testResetAndSeed() throws SQLException {
-//		List<String> expectedTables = EXPECTED_TABLES;
-//		List<String> existingTables = DatabaseHelper.getTables();
-//		for (String name : expectedTables) {
-//			assertTrue("table " + name + " should exist", existingTables.contains(name));
-//		}
-//		for (String name : existingTables) {
-//			assertTrue("table " + name + " should not exist", expectedTables.contains(name));
-//		}
+		List<String> expectedTables = EXPECTED_TABLES;
+		List<String> existingTables = DatabaseHelper.getTables();
+		for (String name : expectedTables) {
+			assertTrue("table " + name + " should exist", existingTables.contains(name));
+		}
+		for (String name : existingTables) {
+			assertTrue("table " + name + " should not exist", expectedTables.contains(name));
+		}
 	}
 
 	@Test
 	public void testInserts() throws SQLException, InvalidStateException {
-//		assertEquals("users should be created", 3, DatabaseHelper.getCount("users"));
-//		assertEquals("projects should be created", 4, DatabaseHelper.getCount("projects"));
+		assertEquals("users should be created", 3, DatabaseHelper.getCount("user"));
+		assertEquals("funding amounts should be created", 8, DatabaseHelper.getCount("funding_amount"));
 	}
 
 }
